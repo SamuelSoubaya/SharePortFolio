23d23cf33eb706f5947ae4cc3ed3d7a0c49dbcf0
souba <souba@LAPTOP-63MVEC3U>
2021-03-08 11:36:54 +0100

    first configuration

---
+++ src/test/java/tp10/metier/ActionTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp10.metier;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author souba
+ */
+public class ActionTest {
+    
+    public ActionTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
